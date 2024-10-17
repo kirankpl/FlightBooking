e542cb623f556e7e8cb487457816693b9ef2416e
Kiran Perunthattil <Kiran_Perunthattil@comcast.com>
2024-10-17 18:10:12 +0530

    update driver path

--- src/main/java/utilities/WebDriverManager.java
+++ src/main/java/utilities/WebDriverManager.java
@@ -2,18 +2,14 @@ package main.java.utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverManager {
-
-    private static WebDriver driver;
-
     public static WebDriver getDriver() {
-        if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-            driver = new ChromeDriver();
-        }
-        return driver;
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
     }
+}
 
     public static void quitDriver() {
         if (driver != null) {
