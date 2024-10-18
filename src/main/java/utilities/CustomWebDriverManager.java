0d391416f5be9e7984d08565ebe9e9c4f4781667
Kiran Perunthattil <Kiran_Perunthattil@comcast.com>
2024-10-18 18:38:03 +0530

    Updated webriver manaager file headless

--- src/main/java/utilities/CustomWebDriverManager.java
+++ src/main/java/utilities/CustomWebDriverManager.java
@@ -1,16 +1,22 @@
 package main.java.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CustomWebDriverManager {
+
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Headless mode for CI
+            options.addArguments("--no-sandbox"); // Required for CI
+            options.addArguments("--disable-dev-shm-usage"); // Avoid limited resource errors
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
